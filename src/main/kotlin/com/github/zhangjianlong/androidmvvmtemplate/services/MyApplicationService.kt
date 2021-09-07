90d953c83e99d22a5d3051e2e6a3a18e9f001cb3
GitHub Action <action@github.com>
2021-09-07 07:59:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zhangjianlong/androidmvvmtemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zhangjianlong.androidmvvmtemplate.services
+
+import com.github.zhangjianlong.androidmvvmtemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/zhangjianlong/androidmvvmtemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zhangjianlong/androidmvvmtemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhangjianlong.androidmvvmtemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhangjianlong.androidmvvmtemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
